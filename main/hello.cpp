8b4d2ab39fadd6380569c8d0d3642edd91597fa0
Sanjana B T <124979904+SanjanaGowda1123@users.noreply.github.com>
2024-03-08 10:18:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Webbok successfully enabled\n";
+  cout << "Webbok successfully  enabled\n";
   return 0;
 }
